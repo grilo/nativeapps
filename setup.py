48c7ab3f3673d5b327583ef86be5eed07b160a26
Joao Grilo <joao.grilo@gmail.com>
2019-10-17 23:40:07 +0200

    Remove previous template's strings.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-    ing.nativeapps web application.
+    nativeapps web application.
 
     Stores mobile applications and generates a list to be downloaded,
     mainly for testing purposes.
@@ -9,36 +9,35 @@
 
 from setuptools import setup, find_packages
 
-# ING local packages should always start with 'ing.'
 setup(
-    name='ing.nativeapps',
-    version='0.1',
-    description='Mobile applications index for testing.',
-    url='https://gitlab.ing.net/ContinuousDeliverySpain/nativeapps',
+    name="nativeapps",
+    version="0.1",
+    description="Mobile applications index for testing.",
+    url="https://github.com/grilo/nativeapps",
     classifiers=[
-        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 2.7",
     ],
     packages=find_packages(),
     package_data={
-        'nativeapps': ['static/*', 'templates/*', 'service/*'],
+        "nativeapps": ["static/*"],
     },
     install_requires=[
-        'flask',
-        'biplist',
-        'AxmlParserPY',
+        "flask",
+        "biplist",
+        "AxmlParserPY",
     ],
     entry_points={
-        'console_scripts': [
-            'nativeapps=nativeapps.cli:cli',
+        "console_scripts": [
+            "nativeapps=nativeapps.cli:cli",
         ],
 
     },
     tests_require=[
-        'pytest-runner',
-        'pylint',
-        'pytest',
-        'pytest-cov',
-        'pytest-mock',
-        'pytest-xdist'
+        "pytest-runner",
+        "pylint",
+        "pytest",
+        "pytest-cov",
+        "pytest-mock",
+        "pytest-xdist"
     ],
 )
